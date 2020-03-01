288e764c2bd3210133e4f35c13bf105cae6859f0
Nazanin <nazanin.dehghani@irisa.fr>
2020-03-01 16:23:43 +0100

    10

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
-
+from mysql_helper import get_items_deleted_time
 app = Flask(__name__)
 
 @app.route('/')
 def show_regions_list():
-    return ' '.join(['شهید عراقی' ,'امیرآباد', 'گیشا'])
+    result = get_items_deleted_time(0)
+    return str(result[0])
+    # return ' '.join(['شهید عراقی' ,'امیرآباد', 'گیشا'])
 
